1503f1618233a759930b00cdee4f9cecd5755f6b
delfiobradors <delfiobradors@users.noreply.github.com>
2015-05-18 23:04:46 +0200

    added functionalities
    Adds the pct of team_home and team_away on last X matches
    Add possibility to filter by teams and competitions
    New file return_percent_given_2teams

--- dates_conversion_ESPN.py
+++ dates_conversion_ESPN.py
@@ -55,8 +55,6 @@ def filter_involved_teams_older_matches(df,ind):
     return x
 
 def add_last_matches_any(df,num_matches):
-    #add column for corner before 9 true or false
-    df['corner9']=df.minc1<10
     #add column for adding corners on last X matches when any of both teams were involved
     df['last_matches_any']=np.nan
     for index in range(0,len(df)-1):
@@ -74,15 +72,97 @@ def add_last_matches_any(df,num_matches):
         #add this value to df
         df.loc[index,'last_matches_any'] = percent
     return df
+    
+def filter_team_home_older_matches(df,ind):
+#select the name of the team
+    team_home=df.iloc[ind].team_home
+    date_of_row=df.iloc[ind].date
+    #select all the rows where one of the teams is involved
+    team_list=[team_home]
+    x = df[(df['team_home'].isin(team_list)) | (df['team_away'].isin(team_list))]
+    #eliminate the newer dates than the studied date
+    x=x[x.date < date_of_row]
+    #sort with the newest matches on top
+    x=x.sort_index(by=['date'], ascending=[False])
+    return x
+    
+def add_last_matches_team_home(df,num_matches):
+    #add column for adding corners on last X matches when any of both teams were involved
+    df['last_matches_team_home']=np.nan
+    for index in range(0,len(df)-1):
+        #given a row of the dataframe, filter the dataframe only with the involved teams
+        df2=filter_team_home_older_matches(df,index)
+        #select how many matches should we look back, and select only those
+        df2=df2.head(num_matches)
+        try:
+            #calculate percentage of trues
+            num_true=float(len(df2[df2.corner9==True]))
+            num_tot=float(len(df2))
+            percent=float(num_true/num_tot)*float(100)
+        except:
+            percent=np.nan
+        #add this value to df
+        df.loc[index,'last_matches_team_home'] = percent
+    return df
+
+def filter_team_away_older_matches(df,ind):
+#select the name of the team
+    team_away=df.iloc[ind].team_away
+    date_of_row=df.iloc[ind].date
+    #select all the rows where one of the teams is involved
+    team_list=[team_away]
+    x = df[(df['team_home'].isin(team_list)) | (df['team_away'].isin(team_list))]
+    #eliminate the newer dates than the studied date
+    x=x[x.date < date_of_row]
+    #sort with the newest matches on top
+    x=x.sort_index(by=['date'], ascending=[False])
+    return x
+    
+def add_last_matches_team_away(df,num_matches):
+    #add column for adding corners on last X matches when any of both teams were involved
+    df['last_matches_team_away']=np.nan
+    for index in range(0,len(df)-1):
+        #given a row of the dataframe, filter the dataframe only with the involved teams
+        df2=filter_team_away_older_matches(df,index)
+        #select how many matches should we look back, and select only those
+        df2=df2.head(num_matches)
+        try:
+            #calculate percentage of trues
+            num_true=float(len(df2[df2.corner9==True]))
+            num_tot=float(len(df2))
+            percent=float(num_true/num_tot)*float(100)
+        except:
+            percent=np.nan
+        #add this value to df
+        df.loc[index,'last_matches_team_away'] = percent
+    return df
+
+
 #read file
 df=pd.read_csv('corners_append.csv')
+
+#PREPARE THE FILE TO RUN FUNCTIONS TO ADD COLUMNS OF LAST MATCHES PCT
+
+#filter chosen competitions
+competition_list=['SPANISH PRIMERA DIVISIÓN','BARCLAYS PREMIER LEAGUE','ITALIAN SERIE A']
+df = df[(df['competition'].isin(competition_list))]
+#remove any duplicate matches (id)
+df=df.drop_duplicates(cols='id',take_last=True)
+#drop matches without minc1
 df=df.dropna(subset = ['minc1'])
+#add column for corner before 9 true or false
+df['corner9']=df.minc1<10
+#convert dates to be able to sort and compare
 convert_dates(df)
+#reset indexes to be able to run the for loop
 df = df.reset_index(drop=True)
 print df
-
 #calculate and add last matches
 df=add_last_matches_any(df,30)
+print df
+df=add_last_matches_team_home(df,30)
+df=add_last_matches_team_away(df,30)
+df=df.sort_index(by=['date'], ascending=[False])
 print df
 
 #fer el mateix pero per els ultims x partits en els que ha estat involucrat team_home i team_away
---
+++ return_percent_given_2teams.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 22:42:15 2015
+
+@author: delfi
+"""
+import pandas as pd
+import numpy as np
+import time
+import datetime
+
+def convert_dates(analysis_df):
+    analysis_df.date=analysis_df.date.apply(convert_to_datetime)
+    
+    #this will return dates converted to ordinal in order to be able to filter easier
+    #analysis_df.date=analysis_df.date.apply(convert_toordinal)
+
+def convert_to_datetime(date):
+    string2=(date[4:15])
+    #if there is a problem will return date 1999-01-01
+    try:
+        conv_time=time.strptime(string2,'%d %b %Y')
+        d=datetime.date(conv_time.tm_year, conv_time.tm_mon, conv_time.tm_mday)
+    except:
+        d=datetime.date(1999, 1, 1)
+    return d
+
+def filter_two_teams(df,team_list):
+    #select all the rows where one of the teams is involved
+    x = df[(df['team_home'].isin(team_list)) | (df['team_away'].isin(team_list))]
+    #sort with the newest matches on top
+    x=x.sort_index(by=['date'], ascending=[False])
+    return x
+
+def return_pct_last_matches(df,num_matches,team_list):
+    #given a row of the dataframe, filter the dataframe only with the involved teams
+    df2=filter_two_teams(df,team_list)
+    #select how many matches should we look back, and select only those
+    df2=df2.head(num_matches)
+    try:
+        #calculate percentage of trues
+        num_true=float(len(df2[df2.corner9==True]))
+        num_tot=float(len(df2))
+        percent=float(num_true/num_tot)*float(100)
+    except:
+        percent=np.nan
+    return percent
+
+df=pd.read_csv('corners_append.csv')
+#PREPARE THE FILE TO RUN FUNCTIONS TO ADD COLUMNS OF LAST MATCHES PCT
+
+#filter chosen competitions
+competition_list=['SPANISH PRIMERA DIVISIÓN','BARCLAYS PREMIER LEAGUE','ITALIAN SERIE A']
+df = df[(df['competition'].isin(competition_list))]
+#remove any duplicate matches (id)
+df=df.drop_duplicates(cols='id',take_last=True)
+#drop matches without minc1
+df=df.dropna(subset = ['minc1'])
+#add column for corner before 9 true or false
+df['corner9']=df.minc1<10
+#convert dates to be able to sort and compare
+convert_dates(df)
+#reset indexes to be able to run the for loop
+df = df.reset_index(drop=True)
+
+team_list=['[Barcelona]','[Almeria]']
+print return_pct_last_matches(df,30,team_list)
